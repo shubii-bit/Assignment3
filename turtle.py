2f813a90ecf2e7c778977caa465e31e72aac681e
Shubham Shrivastava <60490566+shubii-bit@users.noreply.github.com>
2021-01-15 02:20:09 +0530

    Add files via upload

---
+++ turtle.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 15 02:15:42 2021
+
+@author: hp
+"""
+
+import turtle
+t=turtle.Turtle()
+tri=turtle.Screen()
+t.forward(300)
+t.up()
+t.goto(0,0)
+t.down()
+t.left(60)
+t.forward(300)
+t.up()
+t.goto(0,0)
+t.forward(250)
+t.down()
+t.goto(300,0)
+t.hideturtle()
+turtle.done()
